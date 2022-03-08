cc63ce8ec0053e4db355e55829a20adf5503475f
fcfahl <fcfahl@gmx.com>
2022-03-08 13:25:42 +0100

    normalized version

--- salesman.py
+++ salesman.py
@@ -2,10 +2,43 @@ import numpy as np
 import pygad
 import pandas as pd 
 
+
+def convert_array_to_list(array):
+
+    array_list = []
+
+    for l in array:
+        array_list.extend(l)
+
+    return array_list
+
+
+def find_smallest_distance(array):
+
+    array_list = convert_array_to_list(array)
+    
+    array_list.sort()
+    mylist = list(dict.fromkeys(array_list))
+    
+    min_distance = 0
+    max_distance = 0
+
+    for i in mylist[0:8]:
+        min_distance += i
+
+    for i in mylist[-7:]:
+        max_distance += i
+
+    return min_distance, max_distance
+
+
+
 def fitness(route, index):
     distance = 0.0
     cities_visited = [0]    # adiciona primeira cidade com 0
     # route.append(0)         # adiciona ultima cidade com 0
+
+    min_distance, max_distance = find_smallest_distance (cities)
     
     for n in route:        
 
@@ -16,12 +49,13 @@ def fitness(route, index):
         cities_visited.append(n)
         # print (cities_visited)
 
-    distance += cities[len(cities_visited)-1][0]
+    distance += cities[len(cities_visited)-1][0]    
 
-    return 1/distance
+    return ((distance -min_distance)/(max_distance-min_distance))
 
 # Load data
 cities = pd.read_csv('matrix_distances.csv', header=None).values.tolist()
+
 
 # Estrutura da solucao
 gene_space              = [1,2,3,4,5,6,7]
@@ -59,14 +93,14 @@ ga_instance = pygad.GA(
     fitness_func=fitness_func,
 )
 
-ga_instance.run()
-ga_instance.plot_fitness()
-ga_instance.plot_genes()
+# ga_instance.run()
+# ga_instance.plot_fitness()
+# ga_instance.plot_genes()
 
 
-solution, solution_fitness, solution_idx = ga_instance.best_solution()
-print("Parameters of the best solution : {solution}".format(solution=solution))
-print("Fitness value of the best solution = {solution_fitness}".format(solution_fitness=solution_fitness))
+# solution, solution_fitness, solution_idx = ga_instance.best_solution()
+# print("Parameters of the best solution : {solution}".format(solution=solution))
+# print("Fitness value of the best solution = {solution_fitness}".format(solution_fitness=solution_fitness))
 
 # route_test = [3,2,1,4,5,6,7] # nao incluir 0
 # print (fitness(route_test))
